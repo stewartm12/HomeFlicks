7c9aea38fb3a5d34f0314d66bb7b61c5b9a03132
stewartm12 <stewartmorales129@gmail.com>
2021-05-06 00:02:20 -0400

    remove commented codes

--- frontend/components/videos/video_index_item.jsx
+++ frontend/components/videos/video_index_item.jsx
@@ -20,13 +20,9 @@ class VideoIndexItem extends React.Component {
     this.playMiniVideo = this.playMiniVideo.bind(this);
     this.handleResizeWindow = this.handleResizeWindow.bind(this);
     this.togglePlay = this.togglePlay.bind(this);
-    // this.togglePlayPause = this.togglePlayPause.bind(this)
-    // this.displayControls = this.displayControls.bind(this);
     this.removeMovie = this.removeMovie.bind(this);
     this.saveMovie = this.saveMovie.bind(this);
-    // this.volume = this.volume.bind(this);
     this.playerContainer = React.createRef();
-    // this.videoItemDesc = React.createRef();
     this.videoControls = React.createRef();
     this.description = React.createRef();
     this.video = React.createRef();
@@ -35,7 +31,7 @@ class VideoIndexItem extends React.Component {
 
   savedVideo() {
     const {type} = this.props;
-    // 
+    
     if (type === "MY-LIST") {
       return true;
     }
@@ -70,43 +66,11 @@ class VideoIndexItem extends React.Component {
     this.setState({ saved: false });
   }
 
-  // togglePlayPause() {
-  //   if(this.video.current.paused) {
-  //     this.setState({playButton: !this.state.playButton})
-  //     const play = this.video.current.play();
-  //     if (play == undefined) return this.video.current.pause();
-  //     this.video.current.webkitEnterFullScreen();
-  //   } else {
-  //     this.setState({playButton: !this.state.playButton})
-  //     this.video.current.pause();
-  //   }
-  // }
-
   stopMedia() {
     this.video.current.pause();
     this.video.current.currentTime = 0;
     this.setState({ playButton: false });
   }
-
-  // displayControls() {
-  //   if (this.state.hidden) {
-  //     this.setState({hidden: false});
-  //     this.videoControls.current.classList.remove('none');
-  //     this.videoItemDesc.current.classList.remove('desc-hidden')
-  //   } else {
-  //     this.setState({hidden: true});
-  //     this.videoControls.current.classList.add('none')
-  //     this.videoItemDesc.current.classList.add('desc-hidden')
-  //   }
-  // }
-
-  // volume() {
-  //   if(!this.video.current.muted) {
-  //     this.setState({volume: false})
-  //   } else {
-  //     this.setState({volume: true})
-  //   }
-  // }
 
   playMiniVideo(e) {
     if (this.videoTimeOut) {
@@ -118,7 +82,6 @@ class VideoIndexItem extends React.Component {
     }, 2000);
     
     this.videoControls.current.classList.remove("none");
-    // this.videoItemDesc.current.classList.remove("desc-hidden");
   }
 
   stopMiniVideo(e) {
@@ -127,7 +90,6 @@ class VideoIndexItem extends React.Component {
     this.video.current.load();
     this.video.current.pause();
     this.videoControls.current.classList.add("none");
-    // this.videoItemDesc.current.classList.add("desc-hidden");
   }
 
   togglePlay() {
@@ -137,11 +99,10 @@ class VideoIndexItem extends React.Component {
     if (playPromise !== undefined) {
       playPromise.then(() => {
         this.video.current.currentTime = 0;
-        // this.video.current.play();
         this.video.current.requestFullscreen();
         this.setState({ volume: false });
       })
-      .catch(error => console.log("error"))
+      .catch(error => this.video.current.requestFullscreen())
     }
   }
 
@@ -165,23 +126,6 @@ class VideoIndexItem extends React.Component {
         onMouseOver={this.playMiniVideo}
         onMouseLeave={this.stopMiniVideo}
       >
-        {/* <div className="player" onMouseEnter={this.displayControls}  onMouseLeave={this.displayControls} ref={this.playerContainer}>
-          <div className="video-wrapper"> 
-            <video ref={this.video} onTimeUpdate={this.setTime} muted={!this.state.volume}>
-              <source src={this.props.movie.trailer} type="video/mp4"/>
-            </video>
-            <div className="controls none" ref={this.videoControls}>
-              <button onClick={this.togglePlayPause} className="play" aria-label="play pause toggle">{playButton} </button>
-              {saveOrRemove}
-              <button className="sound" onClick={this.volume}>{sound}</button>
-            </div>
-            <div className="video-item-desc desc-hidden" ref={this.videoItemDesc}>
-              <p>{this.props.movie.title}</p>
-              <p>{this.props.movie.year}</p>
-              <p className="video-rating">{this.props.movie.rating}</p>
-            </div>
-          </div>
-        </div> */}
 
         <video
           className="video"
